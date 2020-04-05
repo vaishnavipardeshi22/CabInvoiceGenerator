46d081b431357034926f09a72783de6748d890d5
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-05 22:29:59 +0530

    ADDED : Test method to check minimum fare

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
@@ -13,4 +13,10 @@ public class CabInvoiceGeneratorTest {
         double cabInvoiceGeneratorTotalFare = cabInvoiceGenerator.getTotalFare(5.0, 5);
         Assert.assertEquals(55.0, cabInvoiceGeneratorTotalFare, 0);
     }
+
+    @Test
+    public void givenDistanceAndTime_WhenLessThanMinimumFareGetCalculated_ThenReturnMinimumFare() {
+        double cabInvoiceGeneratorTotalFare = cabInvoiceGenerator.getTotalFare(0.132, 1);
+        Assert.assertEquals(5, cabInvoiceGeneratorTotalFare, 0);
+    }
 }
