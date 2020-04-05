0b010b25d4c99c6e7114eed12baa6a85a166f32a
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-05 21:31:43 +0530

    ADDED : Function to get total fare of the journey

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
+
+    //CONSTANTS
+    private static final double COST_PER_KILOMETER = 10;
+    private static final int COST_PER_MINUTE = 1;
+
+    //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("******************** WELCOME TO CAB INVOICE GENERATOR ********************");
+    }
+
+    //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY
+    public double getTotalFare(double distance, int time) {
+        double totalFare = (COST_PER_KILOMETER * distance) + (COST_PER_MINUTE * time);
+        return totalFare;
     }
 }
