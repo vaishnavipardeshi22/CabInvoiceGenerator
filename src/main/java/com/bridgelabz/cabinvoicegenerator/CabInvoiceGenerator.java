f7539036c715361569523174d83b79307e706b46
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-05 22:31:13 +0530

    REFACTOR : Function to work all the test method

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -5,6 +5,7 @@ public class CabInvoiceGenerator {
     //CONSTANTS
     private static final double COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
+    private static final int MINIMUM_FARE = 5;
 
     //MAIN METHOD
     public static void main(String[] args) {
@@ -14,6 +15,7 @@ public class CabInvoiceGenerator {
     //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY
     public double getTotalFare(double distance, int time) {
         double totalFare = (COST_PER_KILOMETER * distance) + (COST_PER_MINUTE * time);
+        totalFare = Math.max(totalFare, MINIMUM_FARE);
         return totalFare;
     }
 }
