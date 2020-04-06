143a0a854e9e784db16fbce400cdf2e84ae15192
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-06 10:01:13 +0530

    ADDED : Function to get total fare for multiple rides using function overloading

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -12,9 +12,19 @@ public class CabInvoiceGenerator {
         System.out.println("******************** WELCOME TO CAB INVOICE GENERATOR ********************");
     }
 
-    //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY
+    //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY OF SINGLE RIDE
     public double getTotalFare(double distance, int time) {
         double totalFare = (COST_PER_KILOMETER * distance) + (COST_PER_MINUTE * time);
+        totalFare = Math.max(totalFare, MINIMUM_FARE);
+        return totalFare;
+    }
+
+    //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY OF MULTIPLE RIDES
+    public double getTotalFare(Rides[] rides) {
+        double totalFare = 0;
+        for (Rides ride : rides) {
+            totalFare += ride.distance * COST_PER_KILOMETER + ride.time * COST_PER_MINUTE;
+        }
         totalFare = Math.max(totalFare, MINIMUM_FARE);
         return totalFare;
     }
