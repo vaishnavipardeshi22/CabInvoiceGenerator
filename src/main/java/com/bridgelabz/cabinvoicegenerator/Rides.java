86c8e034a2ca5809d556e1ada92380aa5d794b20
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-06 09:59:42 +0530

    ADDED : Model to store multiple rides

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Rides {
+
+    public double distance;
+    public int time;
+
+    public Rides(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
