ac6d7674ac462f75606f04160fe833a0e38c8086
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-08 00:19:47 +0530

    REFACTOR : Code according to ride type

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -3,9 +3,12 @@ package com.bridgelabz.cabinvoicegenerator;
 public class CabInvoiceGenerator {
 
     //CONSTANTS
-    private static final double COST_PER_KILOMETER = 10;
-    private static final int COST_PER_MINUTE = 1;
-    private static final int MINIMUM_FARE = 5;
+    private static final double NORMAL_RIDE_COST_PER_KILOMETER = 10;
+    private static final int NORMAL_RIDE_COST_PER_MINUTE = 1;
+    private static final int NORMAL_RIDE_MINIMUM_FARE = 5;
+    private static final double PREMIUM_RIDE_COST_PER_KILOMETER = 15;
+    private static final int PREMIUM_RIDE_COST_PER_MINUTE = 2;
+    private static final int PREMIUM_RIDE_MINIMUM_FARE = 20;
 
     public RideRepository rideRepository;
 
@@ -18,39 +21,38 @@ public class CabInvoiceGenerator {
         System.out.println("******************** WELCOME TO CAB INVOICE GENERATOR ********************");
     }
 
-    //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY OF SINGLE RIDE
-    public double getTotalFare(double distance, int time) {
-        double totalFare = (COST_PER_KILOMETER * distance) + (COST_PER_MINUTE * time);
-        totalFare = Math.max(totalFare, MINIMUM_FARE);
-        return totalFare;
-    }
-
-    //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY OF MULTIPLE RIDES
+    //FUNCTION TO CALCULATE TOTAL FARE FOR THE JOURNEY OF RIDES ACCORDING TO THEIR TYPE
     public double getTotalFare(Rides[] rides) {
         double totalFare = 0;
         for (Rides ride : rides) {
-            totalFare += ride.distance * COST_PER_KILOMETER + ride.time * COST_PER_MINUTE;
+            if (ride.rideType.equals(Rides.RideType.NORMAL_RIDE)) {
+                totalFare += (ride.distance * NORMAL_RIDE_COST_PER_KILOMETER) + (ride.time * NORMAL_RIDE_COST_PER_MINUTE);
+                totalFare = Math.max(totalFare, NORMAL_RIDE_MINIMUM_FARE);
+            }
+            if (ride.rideType.equals(Rides.RideType.PREMIUM_RIDE)) {
+                totalFare += (ride.distance * PREMIUM_RIDE_COST_PER_KILOMETER) + (ride.time * PREMIUM_RIDE_COST_PER_MINUTE);
+                totalFare = Math.max(totalFare, PREMIUM_RIDE_MINIMUM_FARE);
+            }
         }
-        totalFare = Math.max(totalFare, MINIMUM_FARE);
         return totalFare;
     }
 
-    //FUNCTION TO GET INVOICE SUMMARY BY USING RIDE DATA
-    public InvoiceSummary getInvoiceSummary(Rides[] rides) {
-        double totalFare = getTotalFare(rides);
-        return new InvoiceSummary(rides.length, totalFare);
-    }
+        //FUNCTION TO GET INVOICE SUMMARY BY USING RIDE DATA
+        public InvoiceSummary getInvoiceSummary (Rides[]rides){
+            double totalFare = getTotalFare(rides);
+            return new InvoiceSummary(rides.length, totalFare);
+        }
 
-    //FUNCTION TO GET INVOICE SUMMARY BY USING USER
-    public InvoiceSummary getInvoiceSummary(String userId) {
-        Rides[] rideList = rideRepository.getRides(userId);
-        double totalFare = getTotalFare(rideList);
-        return new InvoiceSummary(rideList.length, totalFare);
-    }
+        //FUNCTION TO GET INVOICE SUMMARY BY USING USER
+        public InvoiceSummary getInvoiceSummary (String userId){
+            Rides[] rideList = rideRepository.getRides(userId);
+            double totalFare = getTotalFare(rideList);
+            return new InvoiceSummary(rideList.length, totalFare);
+        }
 
-    //FUNCTION TO ADD RIDES
-    public void addRides(String userId, Rides[] rides) {
-        rideRepository.addRides(userId, rides);
-    }
+        //FUNCTION TO ADD RIDES
+        public void addRides (String userId, Rides[]rides){
+            rideRepository.addRides(userId, rides);
+        }
 
-}
+    }
