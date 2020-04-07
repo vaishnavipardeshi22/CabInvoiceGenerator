cdafd0e68e36d24710991354705f68b4e76c7055
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-07 10:33:39 +0530

    ADDED : Function to get invoice summary of particular user

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -7,6 +7,12 @@ public class CabInvoiceGenerator {
     private static final int COST_PER_MINUTE = 1;
     private static final int MINIMUM_FARE = 5;
 
+    public RideRepository rideRepository;
+
+    public CabInvoiceGenerator() {
+        this.rideRepository = new RideRepository();
+    }
+
     //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("******************** WELCOME TO CAB INVOICE GENERATOR ********************");
@@ -29,9 +35,22 @@ public class CabInvoiceGenerator {
         return totalFare;
     }
 
-    //FUNCTION TO GET INVOICE SUMMARY
+    //FUNCTION TO GET INVOICE SUMMARY BY USING RIDE DATA
     public InvoiceSummary getInvoiceSummary(Rides[] rides) {
         double totalFare = getTotalFare(rides);
         return new InvoiceSummary(rides.length, totalFare);
     }
+
+    //FUNCTION TO GET INVOICE SUMMARY BY USING USER
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        Rides[] rideList = rideRepository.getRides(userId);
+        double totalFare = getTotalFare(rideList);
+        return new InvoiceSummary(rideList.length, totalFare);
+    }
+
+    //FUNCTION TO ADD RIDES
+    public void addRides(String userId, Rides[] rides) {
+        rideRepository.addRides(userId, rides);
+    }
+
 }
