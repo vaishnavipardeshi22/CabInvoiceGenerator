f8ca428c6a8972351a35ce8a8a05abc6e7e1f548
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-08 00:16:10 +0530

    ADDED : Ride type

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
@@ -4,9 +4,13 @@ public class Rides {
 
     public double distance;
     public int time;
+    RideType rideType;
 
-    public Rides(double distance, int time) {
+    public Rides(RideType rideType, double distance, int time) {
+        this.rideType = rideType;
         this.distance = distance;
         this.time = time;
     }
+
+    public enum RideType {NORMAL_RIDE, PREMIUM_RIDE}
 }
